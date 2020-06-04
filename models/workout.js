69277eb24e3e3e441912e76dc44502a315b25daf
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-06-04 17:59:53 -0400

    update route to include exercise route

--- models/workout.js
+++ models/workout.js
@@ -1,19 +1,55 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema ({
+const WorkoutSchema = new Schema ({
     day: {
         type: Date,
         default: Date.now
     },
     exercises: [
         {
-        type: Schema.Types.ObjectId,
-        ref: "Exercise"
+            type: {
+                type: String,
+                trim: true,
+                required: "Exercise Type:"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Exercise Name:",
+            },
+            duration: {
+                type: Number,
+                required: "Exercise Duration:"
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            }
         }
     ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 module.exports = Workout;
 
